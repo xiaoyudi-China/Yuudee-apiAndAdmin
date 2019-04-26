4ae64789e956ee66f8537db84e245b95988a5082
gaoyiqiang <gaoyiqiang@daokoudai.com>
2019-04-26 19:12:47 +0800

    SystemAppControllerTest unit

--- src/test/java/com/dkd/api/SystemAppControllerTest.java
+++ src/test/java/com/dkd/api/SystemAppControllerTest.java
@@ -1,6 +1,7 @@
 package com.dkd.api;
 
 import com.dkd.XiaoyudiApplication;
+import net.sf.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = XiaoyudiApplication.class, properties = "/application.properties")
@@ -31,9 +33,31 @@ public class SystemAppControllerTest {
     private SystemAppController systemAppController;
     private String reqUrl = "/app/system";
 
+
+    private String loginReqUrl = "/app/user";
+
+    private  String token;
+    private  String mobile="18844272334";
+    @Autowired
+    private WebApplicationContext webApplicationContext;
     @Before
     public void setUp() throws Exception {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(systemAppController).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        ResultActions resultActions = this.mockMvc.
+                perform(MockMvcRequestBuilders.post(loginReqUrl + "/generalLogin")
+                        .param("phone",mobile)
+                        .param("password","123456")
+                        .param("qcellcoreId","1")
+                );
+        MvcResult mvcResult = resultActions.andReturn();
+        logger.info("=====generalLogin mvcResult:" + mvcResult.getResponse().getStatus());
+        String result = mvcResult.getResponse().getContentAsString();
+        logger.info("=====generalLogin result :" + result);
+        JSONObject jsonObject = JSONObject.fromObject(result);
+        String  tokenJson =  jsonObject.optString("data");
+        String parents =  JSONObject.fromObject(tokenJson).optString("parents");
+        token =  JSONObject.fromObject(parents).optString("token");
+        logger.info("=====generalLogin result token:" + token);
     }
 
     @After
@@ -86,6 +110,16 @@ public class SystemAppControllerTest {
         System.out.println("=====productInfo mvcResult:" + mvcResult.getResponse().getStatus());
         String result = mvcResult.getResponse().getContentAsString();
         System.out.println("=====productInfo result:" + result);
+
+        ResultActions resultActionsOther = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/productInfo")
+                        .param("type","89999")
+
+                );
+        MvcResult mvcResultOther = resultActionsOther.andReturn();
+        System.out.println("=====productInfo mvcResult:" + mvcResultOther.getResponse().getStatus());
+        String resultOther = mvcResultOther.getResponse().getContentAsString();
+        System.out.println("=====productInfo result:" + resultOther);
     }
 
     @Test
@@ -101,6 +135,32 @@ public class SystemAppControllerTest {
         System.out.println("=====getAboutUs mvcResult:" + mvcResult.getResponse().getStatus());
         String result = mvcResult.getResponse().getContentAsString();
         System.out.println("=====getAboutUs result:" + result);
+
+
+        ResultActions resultActionsOther = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/aboutUs")
+                        .param("versionsNumber","1")
+                        .param("type","1")
+                        .param("token",token)
+
+                );
+        MvcResult mvcResultOther = resultActionsOther.andReturn();
+        System.out.println("=====getAboutUs mvcResult:" + mvcResultOther.getResponse().getStatus());
+        String resultOther = mvcResultOther.getResponse().getContentAsString();
+        System.out.println("=====getAboutUs result:" + resultOther);
+
+
+        ResultActions resultActionsOtherTwo = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/aboutUs")
+                        .param("versionsNumber","1.0")
+                        .param("type","2")
+                        .param("token",token)
+
+                );
+        MvcResult mvcResultOtherTwo = resultActionsOtherTwo.andReturn();
+        System.out.println("=====getAboutUs mvcResult:" + mvcResultOtherTwo.getResponse().getStatus());
+        String resultOtherTwo = mvcResultOtherTwo.getResponse().getContentAsString();
+        System.out.println("=====getAboutUs result:" + resultOtherTwo);
     }
 
     @Test
@@ -114,6 +174,18 @@ public class SystemAppControllerTest {
         System.out.println("=====getAppStartImage mvcResult:" + mvcResult.getResponse().getStatus());
         String result = mvcResult.getResponse().getContentAsString();
         System.out.println("=====getAppStartImage result:" + result);
+
+
+        ResultActions resultActionsOther = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/appStartImage")
+                        .param("type","10")
+
+                );
+        MvcResult mvcResultOther = resultActionsOther.andReturn();
+        System.out.println("=====getAppStartImage mvcResult:" + mvcResultOther.getResponse().getStatus());
+        String resultOther = mvcResultOther.getResponse().getContentAsString();
+        System.out.println("=====getAppStartImage result:" + resultOther);
+
     }
 
    @Test
@@ -130,6 +202,19 @@ public class SystemAppControllerTest {
         System.out.println("=====getCityList result:" + result);
     }
 
+
+    @Test
+    public void getThisCityList() throws Exception{
+        ResultActions resultActions = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/getThisCityList")
+                );
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("=====getThisCityList mvcResult:" + mvcResult.getResponse().getStatus());
+        String result = mvcResult.getResponse().getContentAsString();
+        System.out.println("=====getThisCityList result:" + result);
+    }
+
+
     @Test
     public void verifyPhone() throws Exception{
         ResultActions resultActions = this.mockMvc.
@@ -141,5 +226,36 @@ public class SystemAppControllerTest {
         System.out.println("=====verifyPhone mvcResult:" + mvcResult.getResponse().getStatus());
         String result = mvcResult.getResponse().getContentAsString();
         System.out.println("=====verifyPhone result:" + result);
+
+
+        ResultActions resultActionsOther = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/verify/phoneNumber")
+                        .param("phone","")
+                        .param("qcellcoreId","1")
+                );
+        MvcResult mvcResultOther = resultActionsOther.andReturn();
+        System.out.println("=====verifyPhone mvcResult:" + mvcResultOther.getResponse().getStatus());
+        String resultOther = mvcResultOther.getResponse().getContentAsString();
+        System.out.println("=====verifyPhone result:" + resultOther);
+
+        ResultActions resultActionsOtherTwo = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/verify/phoneNumber")
+                        .param("phone","17795591253")
+                        .param("qcellcoreId","")
+                );
+        MvcResult mvcResultOtherTwo = resultActionsOtherTwo.andReturn();
+        System.out.println("=====verifyPhone mvcResult:" + mvcResultOtherTwo.getResponse().getStatus());
+        String resultOtherTwo = mvcResultOtherTwo.getResponse().getContentAsString();
+        System.out.println("=====verifyPhone result:" + resultOtherTwo);
+
+        ResultActions resultActionsOtherThree = this.mockMvc.
+                perform(MockMvcRequestBuilders.request(HttpMethod.POST,reqUrl+"/verify/phoneNumber")
+                        .param("phone","17795591253")
+                        .param("qcellcoreId","999")
+                );
+        MvcResult mvcResultOtherThree = resultActionsOtherThree.andReturn();
+        System.out.println("=====verifyPhone mvcResult:" + mvcResultOtherThree.getResponse().getStatus());
+        String resultOtherThree = mvcResultOtherThree.getResponse().getContentAsString();
+        System.out.println("=====verifyPhone result:" + resultOtherThree);
     }
 }
